79dbb40a367500e5312cb22581b7de641527764d
Sean McCabe <sean@ulation.com>
2018-11-30 16:52:08 -0800

    Cleanup

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for f in ./*.md; do
-	aspell --home-dir=. check $f
+for typo in ./*.md; do
+	aspell --home-dir=. check $typo
 
 #Convert from Markdown to LaTeX
 #https://pandoc.org/getting-started.html
-pandoc README.md -f markdown -t latex -s -o cv.tex
-
+	pandoc README.md -f markdown -t latex -s -o resume.tex
+	pandoc resume.tex -t latex -o test.pdf
 done
