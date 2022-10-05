6d5441c85f19ad7e1a46fc66198a30538efed92d
Sean McCabe <sean@ulation.com>
2022-10-05 14:21:52 -0700

    updating publish

--- publish.sh
+++ publish.sh
@@ -5,8 +5,11 @@ for typo in ./*.md; do
 
 #Convert from Markdown to LaTeX
 #https://pandoc.org/getting-started.html
+
+#REQUIRED: 
 # brew install basictex
-pandoc resume.md -o resume.pdf "-fmarkdown-implicit_figures -o" --from=markdown -V geometry:margin=.4in --highlight-style=espresso
+rm SeanMcCabe*
+pandoc resume.md --pdf-engine=/Library/TeX/texbin/pdflatex -o SeanMcCabe-"$(date +'%m-%Y')".pdf "-fmarkdown-implicit_figures -o" --from=markdown -V geometry:margin=.4in --highlight-style=espresso
 	#pandoc resume.md -f markdown -t latex -s -o resume.tex
 	#pandoc resume.tex -t latex -o test.pdf
 done
